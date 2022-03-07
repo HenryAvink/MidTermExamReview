3f8711686b8a438f5ff7c61ce24a0deb91b540b8
Henry Avink <henryscomputerscience@gmail.com>
2022-03-07 14:00:08 -0500

    Added line break at end of Greeting

--- MidTermExamReview.py
+++ MidTermExamReview.py
@@ -1,4 +1,5 @@
-print("Hello! My name is Henry Avink and I am currently a 9th Grader at Gull Lake High School")
+
+print("Hello! My name is Henry Avink and I am currently a 9th Grader at Gull Lake High School\n")
 
 print("One hobby I have is playing VR.\nI really enjoy playing social games in VR. I find it super interesting and I do believe that VR will be the future of gaming.\nI currently have a headset called the Valve Index. The Valve Index is basically the top of the line consumer VR headset.")
 

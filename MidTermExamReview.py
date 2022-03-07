88f98a6da3bc3e5f7e62a5e45398807606f2d11c
Henry Avink <henryscomputerscience@gmail.com>
2022-03-07 13:58:58 -0500

    Added line break at end of line

--- MidTermExamReview.py
+++ MidTermExamReview.py
@@ -1,1 +1,1 @@
-print("Hello! My name is Henry Avink and I am currently a 9th Grader at Gull Lake High School")
+print("Hello! My name is Henry Avink and I am currently a 9th Grader at Gull Lake High School\n")
